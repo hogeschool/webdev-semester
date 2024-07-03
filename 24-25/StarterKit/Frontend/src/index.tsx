bea31a8552ace1a8bbe793846a6e235add9dda3f
Steven <s.koerts2@gmail.com>
2024-07-03 14:06:01 +0200

    Remove Frontend files

--- 24-25/StarterKit/Frontend/src/index.tsx
+++ 24-25/StarterKit/Frontend/src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router'
+import Home from "./pages/Home";
 
 createRoot(document.getElementById('root')!)
     .render(<React.StrictMode>
-        <RouterProvider router={router} />
+        <Home />
     </React.StrictMode>)
